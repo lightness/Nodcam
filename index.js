f5aee311f98a654bbc3ebf7ff3e3890b78820ea0
Uladzimir Aleshka <vladimir_oleshko@smartexlab.com>
2018-05-25 09:34:14 +0000

    Added infinite scroll. Now after command chat scrolls down.

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var WebSocket = require('ws');
 
 
 if (!String.prototype.startsWith) {
-  Object.defineProperty(String.prototype, 'startsWith', {
-    enumerable: false,
-    configurable: false,
-    writable: false,
-    value: function(searchString, position) {
-      position = position || 0;
-      return this.indexOf(searchString, position) === position;
-    }
-  });
+	Object.defineProperty(String.prototype, 'startsWith', {
+		enumerable: false,
+		configurable: false,
+		writable: false,
+		value: function (searchString, position) {
+			position = position || 0;
+			return this.indexOf(searchString, position) === position;
+		}
+	});
 }
 
 
@@ -22,69 +22,69 @@ var app = express();
 
 app.use('/public', express.static('public'));
 
-app.get('/', function(req, res){
-    res.sendFile('./index.html', {root: __dirname});
+app.get('/', function (req, res) {
+	res.sendFile('./index.html', { root: __dirname });
 });
 
-app.listen(API_PORT, function(){
-    console.log('App listening on port 3000!')
+app.listen(API_PORT, function () {
+	console.log('App listening on port 3000!')
 });
 
 
 
 var redis = require("redis"),
-    redisClient = redis.createClient();
- 
+	redisClient = redis.createClient();
+
 redisClient.on("error", function (err) {
-    console.log("Error " + err);
+	console.log("Error " + err);
 });
- 
+
 
 
 
 
 const wss = new WebSocket.Server({ port: CHAT_PORT });
- 
+
 wss.on('connection', function connection(ws) {
-  var ip = getIP(ws);
+	var ip = getIP(ws);
 
-  ws.on('message', function incoming(message) {
-    console.log('[%s] %s ', ip, message);
+	ws.on('message', function incoming(message) {
+		console.log('[%s] %s ', ip, message);
 
-    if (message[0] === '/') {
-    	let result;
-    	let skipHistory = message.startsWith("/history");
+		if (message[0] === '/') {
+			let result;
+			let skipHistory = message.startsWith("/history");
 
-    	try {
-    		result = processCommand(message, ws);
+			try {
+				result = processCommand(message, ws);
 
-    		Promise.resolve(result)
-    			.then(function (resolvedResult) {
-    				sendTo("System", resolvedResult, ws, { skipHistory: skipHistory });
-    			});
-    	}
-    	catch (e) {
-    		sendTo("System", e.message, ws);
-    	}
+				Promise.resolve(result)
+					.then(function (resolvedResult) {
+						sendTo("System", resolvedResult, ws, { skipHistory: skipHistory });
+					});
+			}
+			catch (e) {
+				sendTo("System", e.message, ws);
+			}
 
-    } else {
-	    broadcast(ip, message);		
-    }
+		} else {
+			broadcast(ip, message);
+		}
 
-  });
- 
-  ws.on('close', function(code, message){
-	console.log( 'Disconnected WebSocket '+ ip +' ('+wss.clients.length+' total)' );
-  });
+	});
 
-  console.log( 'New WebSocket Connection '+ ip +' ('+wss.clients.length+' total)' );
+	ws.on('close', function (code, message) {
+		console.log('Disconnected WebSocket ' + ip + ' (' + wss.clients.length + ' total)');
+	});
+
+	console.log('New WebSocket Connection ' + ip + ' (' + wss.clients.length + ' total)');
 });
 
 function processCommand(command, ws) {
 	if (command == "/help") {
 		return "/help - Help\n/online - Online users";
 	} else if (command == "/online") {
-		return "Online users: " + wss.clients.map(function(client) {
+		return "Online users: " + wss.clients.map(function (client) {
 			var clientIP = client._socket.remoteAddress;
 
 			return clientIP === "127.0.0.1" ? "Admin" : clientIP;
@@ -92,27 +92,24 @@ function processCommand(command, ws) {
 	} else if (command.startsWith("/history")) {
 		let parts = command.split(" ");
 
-		if (parts.length !== 3 || !isNumeric(parts[1]) || !isNumeric(parts[2]) ) {
+		if (parts.length !== 3 || !isNumeric(parts[1]) || !isNumeric(parts[2])) {
 			throw Error("Wrong command. Type `/help history` for more info");
 		}
 
 		return getHistory(parseFloat(parts[1]), parseFloat(parts[2]), getIP(ws));
-	}
-
-
-	else {
+	} else {
 		throw Error("Wrong command. Type `/help` for more info");
 	}
 }
 
 function sendTo(fromWho, what, toWs, options) {
 	let msg = JSON.stringify({
-    	user: fromWho,
-    	message: what,
-    	date: new Date()
-    })
+		user: fromWho,
+		message: what,
+		date: new Date()
+	})
 
-	if (!options || !options.skipHistory){
+	if (!options || !options.skipHistory) {
 		redisClient.rpush("messages:" + getIP(toWs), msg, redis.print);
 	}
 
@@ -120,11 +117,11 @@ function sendTo(fromWho, what, toWs, options) {
 }
 
 function broadcast(fromWho, what) {
-  	wss.clients.forEach(function (client) {
-    	if (client.readyState === WebSocket.OPEN) {
-      		sendTo(fromWho, what, client);
-    	}
-  	});
+	wss.clients.forEach(function (client) {
+		if (client.readyState === WebSocket.OPEN) {
+			sendTo(fromWho, what, client);
+		}
+	});
 };
 
 function getIP(ws) {
@@ -132,17 +129,19 @@ function getIP(ws) {
 }
 
 function isNumeric(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
+	return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
 function getHistory(from, to, ip) {
-	console.log("messages:"+ip, from, to);
+	console.log("messages:" + ip, from, to);
 
 	return new Promise(function (resolve) {
-		redisClient.lrange("messages:"+ip, from, to, (e, data) => {
-			console.log(">>>", data);
+		redisClient.lrange("messages:" + ip, from, to, (e, data) => {
+			// console.log(">>>", data);
 
 			resolve(data);
 		});
 	});
 }
+
+
